d6c946f806fbbac631c5ddf72c999b8f6d867b6a
deepabhyudaya <154480782+deepabhyudaya@users.noreply.github.com>
2024-02-26 03:41:23 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -154,7 +154,7 @@ export default function Home() {
       />
     </div> **/
       
-            <div className="h-full w-full flex items-center justify-center text-center rounded-2xl">
+      /**      <div className="h-full w-full flex items-center justify-center text-center rounded-2xl">
               <Drawer shouldScaleBackground>
                 <DrawerTrigger asChild>
                   <Button variant={'link'}>
@@ -184,7 +184,7 @@ export default function Home() {
                 </DrawerContent>
               </Drawer>
             </div> 
-
+**/
 {/* <div className="h-[50rem] w-full dark:bg-black  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">

822f438aee82579a23fd36613b76a4763c2e34a9
advAltTsx <154480782+advAltTsx@users.noreply.github.com>
2024-03-03 03:30:30 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: 'https://sneakshop.vercel.app/',
+        destination: '/messages',
         permanent: true,
       },
     ]

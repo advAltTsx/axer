deb204b6ea2da3a1158ac5af29da9ea123a7ec7c
advAltTsx <154480782+advAltTsx@users.noreply.github.com>
2024-03-03 03:39:04 +0530

    Update text-generate-effect.tsx

--- components/ui/text-generate-effect.tsx
+++ components/ui/text-generate-effect.tsx
@@ -32,7 +32,7 @@ export const TextGenerateEffect = ({
           return (
             <motion.span
               key={word + idx}
-              className="dark:text-white text-white opacity-0"
+              className="dark:text-white font-nothing text-white opacity-0"
             >
               {word}{" "}
             </motion.span>
@@ -45,7 +45,7 @@ export const TextGenerateEffect = ({
   return (
     <div className={cn("font-bold", className)}>
       <div className="mt-4">
-        <div className=" dark:text-white text-white text-2xl leading-snug tracking-wide">
+        <div className=" dark:text-white text-white font-nothing text-2xl leading-snug tracking-wide">
           {renderWords()}
         </div>
       </div>

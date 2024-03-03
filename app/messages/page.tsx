29f82d4301b71a66544480fa3fa400e4e4455b97
Vasooli Bhai <154480782+advAltTsx@users.noreply.github.com>
2024-03-03 12:03:13 +0530

    Update page.tsx

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -54,58 +54,10 @@ export default function Home() {
   return (
     <main className="flex bg-black min-h-screen flex-col items-center justify-center py-24">
       <FloatingNav navItems={navItems} />
-      {/* <Drawer shouldScaleBackground>
-      <DrawerTrigger asChild>
-        <Button variant={'link'}><h1 className="scroll-m-30 underline p-6 text-2xl font-extrabold tracking-tight lg:text-5xl">
-      SA-B?
-    </h1></Button>
-      </DrawerTrigger>
-      <DrawerContent>
-        <div className="h-[70vh]">
-          <DrawerHeader >
-            <div className='h-auto w-full flex items-center justify-start'>
-            <DrawerClose asChild>
-              <Button variant="ghost"><h1 className=' text-blue-600'>Cancel</h1></Button>
-            </DrawerClose>
-            </div>
-            <div className='flex flex-col items-center justify-center text-center h-full my-10'>
-            <DrawerTitle>Still here</DrawerTitle>
-            <DrawerDescription>How are you?</DrawerDescription>
-            <br />
-            <blockquote className="mt-6">{"Tbh, I fear talking to non-boys."}</blockquote>
-            <blockquote className="mb-6">{"Also, I need your help."}</blockquote>
-            <br />
-            <DrawerTitle>{"I hope you understand"}</DrawerTitle>
-            <DrawerDescription>{"Let's meet IRL"}</DrawerDescription>
-
-            </div>
-          </DrawerHeader>
-          
-          <DrawerFooter>
-            
-          </DrawerFooter>
-        </div>
-      </DrawerContent>
-    </Drawer> */}
-      {/*<div
-      className="h-[400vh] bg-black w-full dark:border dark:border-white/[0.1] rounded-md relative pt-40 overflow-clip"
-      ref={ref}
-    >
-      <GoogleGeminiEffect
-        pathLengths={[
-          pathLengthFirst,
-          pathLengthSecond,
-          pathLengthThird,
-          pathLengthFourth,
-          pathLengthFifth,
-        ]}
-      />
-    </div> */}
-      
            <div className="h-full w-full flex items-center justify-center text-center rounded-2xl">
               <Drawer shouldScaleBackground>
                 <DrawerTrigger asChild>
-                <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none">
+                  <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none">
   <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#076EFF_0%,#FFB7C5_25%,#FFDDB7_50%,#FFB7C5_75%,#076EFF_100%)]" />
   <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-black px-6 py-1 text-sm font-bold text-white backdrop-blur-3xl">
     Maybe someday 
@@ -121,7 +73,7 @@ export default function Home() {
                         </DrawerClose>
                       </div>
                       <div className="flex flex-col justify-center text-center h-full py-10 px-6 pt-16">
-           <DrawerTitle><span className="text-white font-nothing" ><TextGenerateEffect words="I really don't know how to talk to you. You'd just go acting like 'bruh who the hell are you, I didn't even know you existed, well you do, congratulations on that 🎉', so either you start the conversation or give any topic."/></span>
+           <DrawerTitle><span className="text-white font-nothing" ><TextGenerateEffect words="Anyways, all the best"/></span>
            </DrawerTitle>
            
                       </div>

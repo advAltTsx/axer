66c75e5f0b950c9b361fdd16afd5aaea3f845d52
Vasooli Bhai <154480782+advAltTsx@users.noreply.github.com>
2024-03-03 12:04:26 +0530

    Update page.tsx

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
                   <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none">
   <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#076EFF_0%,#FFB7C5_25%,#FFDDB7_50%,#FFB7C5_75%,#076EFF_100%)]" />
   <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-black px-6 py-1 text-sm font-bold text-white backdrop-blur-3xl">
-    Maybe someday 
+    So
   </span>
 </button>
                 </DrawerTrigger>

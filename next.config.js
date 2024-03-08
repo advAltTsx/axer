bd937a669db7fd2cfaf8177d7a3f0ddf7cb78edd
Vasooli Bhai <154480782+advAltTsx@users.noreply.github.com>
2024-03-08 14:25:56 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: '/WorstPersonEver.mp3',
+        destination: '/worstpersonever.mp3',
         permanent: true,
       },
     ]

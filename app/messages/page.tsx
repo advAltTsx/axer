ee958ac167769ce15b5f6fc8ee9027cde023000b
Vasooli Bhai <154480782+advAltTsx@users.noreply.github.com>
2024-03-10 21:02:11 +0530

    Update page.tsx

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -53,24 +53,9 @@ export default function Home() {
  
   return (
     <main className="">
-      {'{"https://advwastaken.vercel.app (community version) has been abandoned since 20:01, 03/08/24"}'}
-   <br/>
-      {'{"reason: Humiliation | Betrayal | Dual Nature of closest IRL friend"}'}
-      <br/>
-      {'{"extreme hates: liars}'} 
-    <br/>
-        
-      {'"Stalkers falsely guessing I\'m talking about SS-B[DAV] currently, not even close, they\'re far more respectable than you, they\'ve even contributed for resizeable screens in v69.6.1 through GitHub, truly an advanced person (not like you, lmfao), why not minding your own business instead?"'}
-   <br/>
-      {'"SS-B[DAV] is my friendliest sister. I would do whatever I want, scream, talk, or text anyone. Who the hell are you to even think of making me scream at them? Be in your limits. It\s a dare to speak something bad about them infront of me ever again."'} <br/>
-     <span className="text-red-600"><s> {'{"tera website bohot bada koi ambani ka website hai bhi nahi joh baar baar dekhne aaye - apni aukaat mein raho tere paas itna bhi nhi h", "tere baap ka website hai kya be *giggles* - tumlog se jyada ganda insaan se aaj takk kabhi mulakat nahi hua", "1 saal se harr ek email dekhe ho aur puchte aur email address kaha se mila - besharam ho ekdm?", "jhuth bolne mein mbbs karke rakhe hai - agar pata rhta toh jab Scarlet ke saath ladai karrha tha tabhi tereko sorry bolne bhi nahi aata, terese jyada accha toh mera dushman log hai, mere dushman bhi mereko dekhke mere samne gaali dena baand krdete hai", "bahiniya, bhutarni - tere jaise ko *aap* bola uske baare mein zindagi bhar sochunga, tumlog jhutho baat karne bhi layak nahi ho, insaaniyat toh durr ka baat hai", "remembering these sentences - I hate even your voice now"'}</s></span>
-    <br/>
-      <span className="text-blue-500 font-bold"> You as well as I, we both know I would never in my life say you like this even mistakenly or in intense anger, just never 🥲
-        <br/>
-        All I told my brother-like oldest friend was to write something such worse that {"you'd never come here again yourself"} <br/>
-      </span>
-        I made you call your brother to say him that <u>{"you're talented in Typescript/Javascript and we could be a great team that's why I'm looking for you or build up some stories and we'd have been in a team with granted permissions :("} </u>, but I also got angry when you gave the phone to some other kid and I knew beforehands {"there's gonna be a fight"} because of him. Also, keep damn control on your mouth I never expected this from *you* 🥲
+      {'{"https://advwastaken.vercel.app (community version) has been discontinued since 20:01, 03/08/24"}'}
    
+      
     </main>
   );
 }
